1c18405aaf1ab4eb951b78b4bf0ecac6178e7083
Priyank Srivastava <prsrivastava@ad.wynshop.com>
2024-08-25 21:05:17 -0400

    assignment.sh: completed the assignment now

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -11,7 +11,7 @@ set -x
 mkdir dir1 dir2 dir3 dir4 dir5
 
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
-ls ./
+ls
 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
 cd dir2
@@ -21,12 +21,14 @@ touch file1 file2 file3 file4 file5
 echo "Hello world" > file3
 
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
-
+cat file3
 
 # 6. Delete file4
-
+rm file4
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
-
+cd ../
+rm -r dir4 dir5
 
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
+ls ./
